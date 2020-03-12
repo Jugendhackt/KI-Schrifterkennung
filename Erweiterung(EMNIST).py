a6b85ed6ee5ab5dc670d02bf2af9c44bb732b42b
zuvielSkill <franz.schlicht@gmail.com>
2020-03-12 19:11:58 +0100

    Versuch das Dateinset als ein numpy.array zu speicher, um es später in ein 1 dimensionales Array zu "reshapen"
    Fehler beim Erzeugen des Arrays

--- Erweiterung(EMNIST).py
+++ Erweiterung(EMNIST).py
@@ -1,3 +1,15 @@
 # Diese Datei ist nach dem Jugend Hackt Event entstanden
 # Es wurde von Franz Schlicht programmieret
 # Änderungen: EMNIST Dataset wird verwendet(auch Buchstaben)
+
+
+from emnist import extract_training_samples
+import numpy as np
+
+# daten speichern
+digits = np.array(extract_training_samples('digits'))
+print(digits)
+
+# daten eindemensional machen
+n_samples = len(digits)
+#data = digits.reshape(n_sampels, -1)
